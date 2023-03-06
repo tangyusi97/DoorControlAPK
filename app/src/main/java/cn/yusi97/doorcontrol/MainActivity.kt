7e5976eba1372bd814402e71cad0c6a65f3c0b74
Yusi Tang <2080466270@qq.com>
2023-03-06 14:42:14 +0800

    重构-封装振动和广播代码

--- app/src/main/java/cn/yusi97/doorcontrol/MainActivity.kt
+++ app/src/main/java/cn/yusi97/doorcontrol/MainActivity.kt
@@ -1,21 +1,8 @@
 package cn.yusi97.doorcontrol
 
-import android.Manifest
-import android.app.Activity
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothManager
-import android.bluetooth.le.AdvertiseCallback
-import android.bluetooth.le.AdvertiseData
-import android.bluetooth.le.AdvertiseSettings
-import android.bluetooth.le.BluetoothLeAdvertiser
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -25,64 +12,39 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import cn.yusi97.doorcontrol.ui.theme.AppTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 
-var hasAdvertisePermission by mutableStateOf(false)
-var isBluetoothAdapterEnabled by mutableStateOf(false)
+private var hasAdvertisePermission by mutableStateOf(false)
+private var isBluetoothAdapterEnabled by mutableStateOf(false)
 
-var bluetoothAdapter: BluetoothAdapter? = null
-var bluetoothLeAdvertiser: BluetoothLeAdvertiser? = null
-lateinit var advertiseSettings: AdvertiseSettings
-lateinit var advertiseCallback: AdvertiseCallback
+private lateinit var myBleAdvertiser: MyBleAdvertiser
+private lateinit var myVibrator: MyVibrator
 
 class MainActivity : ComponentActivity() {
-    private val ctx = this
-    private var advertiseDenied = false
-    private var openBluetoothDenied = false
 
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) {
-        it.forEach { (permission, isGranted) ->
-            when (permission) {
-                Manifest.permission.BLUETOOTH_ADVERTISE,
-                Manifest.permission.BLUETOOTH -> {
-                    hasAdvertisePermission = isGranted
-                    if (!isGranted) {
-                        advertiseDenied = true
-                        Toast.makeText(this, getString(R.string.no_permission), Toast.LENGTH_SHORT)
-                            .show()
-                    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        myVibrator = MyVibrator(this)
+        myBleAdvertiser = MyBleAdvertiser(this) { event, value ->
+            when (event) {
+                MyBleAdvertiserEvent.ADVERTISE_PERMISSION -> {
+                    hasAdvertisePermission = value
                 }
-                Manifest.permission.BLUETOOTH_CONNECT,
-                Manifest.permission.BLUETOOTH_ADMIN -> {
-                    //
+                MyBleAdvertiserEvent.OPEN_BLE -> {
+                    isBluetoothAdapterEnabled = value
                 }
             }
         }
-    }
 
-    private val openBluetoothLauncher = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) {
-        isBluetoothAdapterEnabled = it.resultCode == Activity.RESULT_OK
-        if (it.resultCode == Activity.RESULT_CANCELED) openBluetoothDenied = true
-    }
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             AppTheme {
                 FullScreen()
@@ -96,106 +58,10 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-        advertiseSettings = AdvertiseSettings.Builder()
-            .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
-            .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
-            .setTimeout(0)
-            .setConnectable(false)
-            .build()
-        advertiseCallback = advertiseCallback1()
-    }
 
     override fun onResume() {
         super.onResume()
-        checkAdvertisePermission { setUpBluetooth() }
-    }
-
-    private fun setUpBluetooth() {
-        hasAdvertisePermission = true
-
-        bluetoothAdapter = getSystemService(BluetoothManager::class.java).adapter
-
-        // 检测蓝牙
-        if (bluetoothAdapter == null) {
-            Toast.makeText(this, getString(R.string.no_bluetooth), Toast.LENGTH_SHORT).show()
-            hasAdvertisePermission = false
-            return
-        }
-        // 初始化广播器
-        bluetoothLeAdvertiser = bluetoothAdapter!!.bluetoothLeAdvertiser
-        // 打开蓝牙
-        if (!bluetoothAdapter!!.isEnabled) {
-            if (!openBluetoothDenied) {
-                checkOpenBluetoothPermission {
-                    val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                    openBluetoothLauncher.launch(enableBluetoothIntent)
-                }
-            }
-        } else {
-            isBluetoothAdapterEnabled = true
-        }
-    }
-
-    private fun checkAdvertisePermission(onPass: () -> Unit) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            if (ContextCompat.checkSelfPermission(
-                    this, Manifest.permission.BLUETOOTH_ADVERTISE
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                onPass()
-            } else if (!advertiseDenied) {
-                requestPermissionLauncher.launch(
-                    arrayOf(Manifest.permission.BLUETOOTH_ADVERTISE)
-                )
-            }
-        } else {
-            if (ContextCompat.checkSelfPermission(
-                    this, Manifest.permission.BLUETOOTH
-                ) == PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(
-                    this, Manifest.permission.BLUETOOTH_ADMIN
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                onPass()
-            } else if (!advertiseDenied) {
-                requestPermissionLauncher.launch(
-                    arrayOf(
-                        Manifest.permission.BLUETOOTH,
-                        Manifest.permission.BLUETOOTH_ADMIN
-                    )
-                )
-            }
-        }
-    }
-
-    private fun checkOpenBluetoothPermission(onPass: () -> Unit) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            if (ContextCompat.checkSelfPermission(
-                    this, Manifest.permission.BLUETOOTH_CONNECT
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                onPass()
-            } else {
-                requestPermissionLauncher.launch(
-                    arrayOf(Manifest.permission.BLUETOOTH_CONNECT)
-                )
-            }
-        } else {
-            onPass()
-        }
-    }
-
-    private fun advertiseCallback1() = object : AdvertiseCallback() {
-        override fun onStartSuccess(settingsInEffect: AdvertiseSettings) {
-            super.onStartSuccess(settingsInEffect)
-        }
-
-        override fun onStartFailure(errorCode: Int) {
-            super.onStartFailure(errorCode)
-            Toast.makeText(ctx, getString(R.string.advertise_failed), Toast.LENGTH_SHORT).show()
-        }
+        myBleAdvertiser.setUp()
     }
 
 }
@@ -257,49 +123,51 @@ fun Title() {
 @Composable
 fun ControlPanel() {
     val enabledList = remember { mutableStateListOf(false, false, false, false) }
+    var disabled by remember { mutableStateOf(false) }
 
     fun enableAll(enabled: Boolean) {
         for (j in 0 until enabledList.size) {
-            enabledList[j] = enabled
+            enabledList[j] = enabled && !disabled
         }
     }
 
     fun setEnabled(i: Int) {
         enableAll(false)
-        enabledList[i] = true
+        enabledList[i] = true && !disabled
     }
 
     LaunchedEffect(hasAdvertisePermission, isBluetoothAdapterEnabled) {
-        enableAll(hasAdvertisePermission && isBluetoothAdapterEnabled)
+        disabled = !(hasAdvertisePermission && isBluetoothAdapterEnabled)
+        enableAll(true)
     }
 
     ControlButton(
-        stringResource(R.string.open),
-        enabledList[0],
-        { setEnabled(0) },
-        { enableAll(true) },
-        DoorActionCmd.open
+        name = stringResource(R.string.open),
+        enabled = enabledList[0],
+        setEnabled = { setEnabled(0) },
+        resetEnabled = { enableAll(true) },
+        cmdData = DoorActionCmd.open
     )
     ControlButton(
-        stringResource(R.string.stop),
-        enabledList[1],
-        { setEnabled(1) },
-        { enableAll(true) },
-        DoorActionCmd.stop
+        name = stringResource(R.string.stop),
+        enabled = enabledList[1],
+        setEnabled = { setEnabled(1) },
+        resetEnabled = { enableAll(true) },
+        cmdData = DoorActionCmd.stop
     )
     ControlButton(
-        stringResource(R.string.close),
-        enabledList[2],
-        { setEnabled(2) },
-        { enableAll(true) },
-        DoorActionCmd.close
+        name = stringResource(R.string.close),
+        enabled = enabledList[2],
+        setEnabled = { setEnabled(2) },
+        resetEnabled = { enableAll(true) },
+        cmdData = DoorActionCmd.close
     )
     ControlButton(
-        stringResource(R.string.open_and_close),
-        enabledList[3],
-        { setEnabled(3) },
-        { enableAll(true) },
-        DoorActionCmd.openAndClose,
+        name = stringResource(R.string.open_and_close),
+        enabled = enabledList[3],
+        setEnabled = { setEnabled(3) },
+        resetEnabled = { enableAll(true) },
+        cmdData = DoorActionCmd.openAndClose,
         true
     )
 }
@@ -313,33 +181,23 @@ fun ControlButton(
     cmdData: ByteArray,
     primary: Boolean? = false,
 ) {
-    val ctx = LocalContext.current
     val interactionSource = remember { MutableInteractionSource() }
     val pressState by interactionSource.collectIsPressedAsState()
     DisposableEffect(pressState) {
-        if (bluetoothAdapter?.isEnabled != true) {
-            isBluetoothAdapterEnabled = false
-        } else if (pressState) {
+        if (pressState) {
             setEnabled()
-            VibrateUtils.vibrate(ctx, longArrayOf(0, 50, 250), 0)
-            val data = AdvertiseData.Builder().addManufacturerData(0xFFF0, cmdData).build()
-            if (ActivityCompat.checkSelfPermission(
-                    ctx,
-                    Manifest.permission.BLUETOOTH_ADVERTISE
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                bluetoothLeAdvertiser?.startAdvertising(advertiseSettings, data, advertiseCallback)
-            }
+            myBleAdvertiser.advertiseCommand(cmdData)
+            myVibrator.vibrate(longArrayOf(0, 50, 250), 0)
 
         } else {
             resetEnabled()
-            VibrateUtils.vibrateCancel(ctx)
-            bluetoothLeAdvertiser?.stopAdvertising(advertiseCallback)
+            myBleAdvertiser.advertiseCancel()
+            myVibrator.cancel()
         }
 
         onDispose {
-            VibrateUtils.vibrateCancel(ctx)
-            bluetoothLeAdvertiser?.stopAdvertising(advertiseCallback)
+            myBleAdvertiser.advertiseCancel()
+            myVibrator.cancel()
         }
     }
 
--- app/src/main/java/cn/yusi97/doorcontrol/Util.kt
+++ app/src/main/java/cn/yusi97/doorcontrol/Util.kt
@@ -1,15 +1,29 @@
 package cn.yusi97.doorcontrol
 
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.AdvertiseCallback
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertiseSettings
+import android.bluetooth.le.BluetoothLeAdvertiser
 import android.content.Context
 import android.content.Context.VIBRATOR_SERVICE
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.os.Build
 import android.os.Vibrator
 import android.os.VibratorManager
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 
 val Number.vh: Dp
     get() {
@@ -26,17 +40,17 @@ val Number.th: TextUnit
     }
 
 @Suppress("deprecation")
-object VibrateUtils {
+class MyVibrator(context: Context) {
 
-    //震动milliseconds毫秒
-    fun vibrate(context: Context, milliseconds: Long) {
-        val vib = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            val vibratorManager =
-                context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-            vibratorManager.defaultVibrator
-        } else {
-            context.getSystemService(VIBRATOR_SERVICE) as Vibrator
-        }
+    private val vib = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        val vibratorManager =
+            context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+        vibratorManager.defaultVibrator
+    } else {
+        context.getSystemService(VIBRATOR_SERVICE) as Vibrator
+    }
+
+    fun vibrate(milliseconds: Long) {
         vib.vibrate(milliseconds)
     }
 
@@ -44,26 +58,11 @@ object VibrateUtils {
      * 以pattern[]方式震动
      * @param repeat -1 不重复  0一直震动
      */
-    fun vibrate(context: Context, pattern: LongArray, repeat: Int) {
-        val vib = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            val vibratorManager =
-                context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-            vibratorManager.defaultVibrator
-        } else {
-            context.getSystemService(VIBRATOR_SERVICE) as Vibrator
-        }
+    fun vibrate(pattern: LongArray, repeat: Int) {
         vib.vibrate(pattern, repeat)
     }
 
-    //取消震动
-    fun vibrateCancel(context: Context) {
-        val vib = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            val vibratorManager =
-                context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-            vibratorManager.defaultVibrator
-        } else {
-            context.getSystemService(VIBRATOR_SERVICE) as Vibrator
-        }
+    fun cancel() {
         try {
             vib.cancel()
         } catch (e: Exception) {
@@ -72,6 +71,170 @@ object VibrateUtils {
     }
 }
 
+enum class MyBleAdvertiserEvent {
+    ADVERTISE_PERMISSION, OPEN_BLE
+}
+
+class MyBleAdvertiser(
+    activity: ComponentActivity,
+    private val eventHandler: (event: MyBleAdvertiserEvent, value: Boolean) -> Unit
+) {
+    private val ctx = activity
+    private var advertiseDenied = false
+    private var openBluetoothDenied = false
+    private val bluetoothAdapter: BluetoothAdapter? =
+        activity.getSystemService(BluetoothManager::class.java).adapter
+    private var bluetoothLeAdvertiser: BluetoothLeAdvertiser? = null
+
+    private val advertiseSettings: AdvertiseSettings = AdvertiseSettings.Builder()
+        .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
+        .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
+        .setTimeout(0)
+        .setConnectable(false)
+        .build()
+
+    private val advertiseCallback = object : AdvertiseCallback() {
+        override fun onStartSuccess(settingsInEffect: AdvertiseSettings) {
+            super.onStartSuccess(settingsInEffect)
+        }
+
+        override fun onStartFailure(errorCode: Int) {
+            super.onStartFailure(errorCode)
+            Toast.makeText(ctx, ctx.getString(R.string.advertise_failed), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun setUp() {
+        // 检测蓝牙支持
+        if (bluetoothAdapter == null) {
+            eventHandler(MyBleAdvertiserEvent.OPEN_BLE, false)
+            Toast.makeText(ctx, ctx.getString(R.string.no_bluetooth), Toast.LENGTH_SHORT).show()
+            return
+        }
+        // 检查广播权限
+        if (!checkAdvertisePermission(!advertiseDenied)) {
+            eventHandler(MyBleAdvertiserEvent.ADVERTISE_PERMISSION, false)
+            Toast.makeText(ctx, ctx.getString(R.string.no_permission), Toast.LENGTH_SHORT).show()
+            return
+        }
+        eventHandler(MyBleAdvertiserEvent.ADVERTISE_PERMISSION, true)
+        // 打开蓝牙开关
+        if (!bluetoothAdapter.isEnabled) {
+            eventHandler(MyBleAdvertiserEvent.OPEN_BLE, false)
+            if (!openBluetoothDenied && checkOpenBluetoothPermission()) {
+                val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                openBluetoothLauncher.launch(enableBluetoothIntent)
+            }
+            return
+        }
+        eventHandler(MyBleAdvertiserEvent.OPEN_BLE, true)
+        // 初始化广播器
+        bluetoothLeAdvertiser = bluetoothAdapter.bluetoothLeAdvertiser
+    }
+
+    fun advertiseCommand(cmdData: ByteArray) {
+        if (bluetoothAdapter?.isEnabled != true) {
+            eventHandler(MyBleAdvertiserEvent.OPEN_BLE, false)
+            Toast.makeText(ctx, ctx.getString(R.string.no_bluetooth), Toast.LENGTH_SHORT).show()
+            return
+        }
+        val data = AdvertiseData.Builder().addManufacturerData(0xFFF0, cmdData).build()
+        if (!checkAdvertisePermission(false)) {
+            eventHandler(MyBleAdvertiserEvent.ADVERTISE_PERMISSION, false)
+            Toast.makeText(ctx, ctx.getString(R.string.no_permission), Toast.LENGTH_SHORT).show()
+        }
+        bluetoothLeAdvertiser?.startAdvertising(advertiseSettings, data, advertiseCallback)
+    }
+
+    fun advertiseCancel() {
+        if (!checkAdvertisePermission(false)) {
+            eventHandler(MyBleAdvertiserEvent.ADVERTISE_PERMISSION, false)
+        }
+        bluetoothLeAdvertiser?.stopAdvertising(advertiseCallback)
+    }
+
+    private fun checkAdvertisePermission(request: Boolean? = false): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (ContextCompat.checkSelfPermission(
+                    ctx, Manifest.permission.BLUETOOTH_ADVERTISE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                return true
+            } else if (request == true) {
+                requestPermissionLauncher.launch(
+                    arrayOf(Manifest.permission.BLUETOOTH_ADVERTISE)
+                )
+            }
+            return false
+        }
+        if (ContextCompat.checkSelfPermission(
+                ctx, Manifest.permission.BLUETOOTH
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(
+                ctx, Manifest.permission.BLUETOOTH_ADMIN
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        } else if (request == true) {
+            requestPermissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN
+                )
+            )
+        }
+        return false
+    }
+
+    private fun checkOpenBluetoothPermission(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (ContextCompat.checkSelfPermission(
+                    ctx, Manifest.permission.BLUETOOTH_CONNECT
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                return true
+            }
+            requestPermissionLauncher.launch(
+                arrayOf(Manifest.permission.BLUETOOTH_CONNECT)
+            )
+            return false
+        }
+        return true
+    }
+
+    private val requestPermissionLauncher = activity.registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) {
+        it.forEach { (permission, isGranted) ->
+            when (permission) {
+                Manifest.permission.BLUETOOTH_ADVERTISE,
+                Manifest.permission.BLUETOOTH -> {
+                    if (!isGranted) {
+                        advertiseDenied = true
+                        Toast.makeText(
+                            ctx,
+                            ctx.getString(R.string.no_permission),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.BLUETOOTH_ADMIN -> {
+                    //
+                }
+            }
+        }
+    }
+
+    private val openBluetoothLauncher = activity.registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == Activity.RESULT_CANCELED) {
+            openBluetoothDenied = true
+            Toast.makeText(ctx, ctx.getString(R.string.no_bluetooth), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
 
 object DoorActionCmd {
     val open = byteArrayOf(
